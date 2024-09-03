1d736b6ee308878d0671cc4c7655ce77f96b2123
javohirhasanov <javohirhasanov3303@gmail.com>
2024-09-04 02:47:11 +0500

    fix file name

--- src/components/GreetingBoard/GreetingBoard.jsx
+++ src/components/GreetingBoard/GreetingBoard.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect, useRef} from 'react';
 import axios from 'axios';
-import './greetingBoard.css';
+import './greetingboard.css';
 import {Loader} from "../Loader/Loader.jsx";
 
 export const GreetingBoard = () => {
